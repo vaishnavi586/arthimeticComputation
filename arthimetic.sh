d85c7bb3bf8bf47100c70141f514a99d67a81621
vaishnavi586 <machavolu1996@gmail.com>
2020-10-27 12:57:46 +0530

    displayed the resulting array elements in descending order

--- arthimetic.sh
+++ arthimetic.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash 
 read -p "enter three inputs" a b c
 echo $a $b $c
 counter=0
@@ -6,7 +6,11 @@ pb[((counter++))]="$(( $a+$b*$c ))"
 pb[((counter++))]="$(( $a*$b+$c ))"
 pb[((counter++))]="$(( $c+$a/$b ))"
 pb[((counter++))]="$(( $a%$b+$c ))"
-echo ${pb[@]}
-echo ${!pb[@]}
-array=${!pb[@]}
-echo ${array[@]}
+echo "dictionary values are " ${pb[@]}
+echo "dictionary keys are" ${!pb[@]}
+array=${pb[@]}
+echo "values in array" ${array[@]}
+sortDescending=`echo ${array[@]} | awk 'BEGIN{RS=" "} {print $1}' |sort -nr `
+echo "decending order is"$sortDescending
+
+
